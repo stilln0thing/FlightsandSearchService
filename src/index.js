67f0cd7514ee985d39d12145f5fe0ece42a5373e
MachineMaven <mathcrayon12@gmail.com>
2024-07-14 23:08:53 +0530

    added basic bodyparsers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async()=>{
+
+    //create the express object
     const app = express();
 
-
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, ()=>{
           console.log(`Server running at ${PORT}`);
             
     })
 }
 
-setupAndStartServer();
+setupAndStartServer();  
