0e7fb1b1e21624969278ad4671736b8565428152
MachineMaven <mathcrayon12@gmail.com>
2024-07-17 11:29:53 +0530

    Completed the city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,19 +6,47 @@ class CityRepository{
             const city = await City.create({name});
             return city;
         } catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
     async deleteCity({ cityId }){
         try{
-             await City.destroy()({
+             await City.destroy({
                 where: {
                     id: cityId
                 }
              });
+             return true;
         } catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try{
+           const city = await City.update(data, {
+            where: {
+                 id: cityId
+            }
+           });
+           return city;
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
+
+    async getCity(cityId){
+        try{
+        const city = await City.findByPk(cityId);
+        return City;
+        }  catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw(error);
         }
     }
 }
