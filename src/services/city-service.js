3c97207d5b3e679fc60a35b639f4401554f9a711
MachineMaven <mathcrayon12@gmail.com>
2024-07-21 17:43:25 +0530

    Added get all cities api

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -40,6 +40,16 @@ class CityService {
          console.log("Something went wrong at service layer");
         }
     }
+
+    async getAllCities(){
+        try {
+            const cities = await this.cityRepository.getAllCities();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw(error);
+        }
+    }
 }
 
 module.exports = CityService;
