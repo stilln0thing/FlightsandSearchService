4682258dba26c365a166879265c6de55b96d2f9e
MachineMaven <mathcrayon12@gmail.com>
2024-07-24 15:14:03 +0530

    added flights model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+const {Airplane} = require('./models/index');
+
 const setupAndStartServer = async()=>{
 
     //create the express object
@@ -14,12 +17,14 @@ const setupAndStartServer = async()=>{
 
     app.use('/api', ApiRoutes);
 
-
-
-
     app.listen(PORT, async ()=>{
           console.log(`Server running at ${PORT}`);
-         
+          if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+          }
+          await Airplane.create({
+            modelNumber: 'Bombardier CRJ'
+          })
     })
 }
 
