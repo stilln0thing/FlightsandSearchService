9a861077f62e9a0967694c64b0c83c824f563442
MachineMaven <mathcrayon12@gmail.com>
2024-07-24 23:51:16 +0530

    glti

--- src/repository/airplane-repository.js
+++ src/repository/airplane-repository.js
@@ -4,6 +4,7 @@ class AirplaneRepository {
     async getAirplane(id){
        try {
          const airplane = await Airplane.findByPk(id);
+        
          return airplane;
        } catch (error) {
         console.log("Something went wrong int the repository layer");
